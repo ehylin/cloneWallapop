6baf64820b60dcfb7663181fb962fc563da1be5e
Ehylin <ehylinolezjua@gmail.com>
2023-10-27 21:21:03 +0200

    add signup

---
+++ session/sessionView.js
@@ -0,0 +1,12 @@
+export const buildUnauthorizedSession = () => {
+    return `<ul>
+    <li>
+      <a href="./login.html">Login</a>
+      <a href="./signup.html">Signup</a>
+    </li>
+  </ul>`;
+}
+
+export const buildAuthenticatedSession = () => {
+    return `<button>Cerrar sesión</button>`;
+}
